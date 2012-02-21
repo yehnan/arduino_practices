9cdea87eca9242883072faefd38dcd582ff16078
Yehnan Chiang <yehnan174@gmail.com>
2012-02-21 10:36:50 +0800

    SwitchLed

---
+++ SwitchLed/SwitchLed.ino
@@ -0,0 +1,32 @@
+static int ledStatus;
+static unsigned long lastDebounceTime;
+#define DEBOUNCE_DELAY 200
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT);
+  
+  ledStatus = LOW;
+  digitalWrite(13, ledStatus);
+}
+
+void updateLed(){
+  unsigned long currentTime = millis();
+  if((currentTime - lastDebounceTime) > DEBOUNCE_DELAY){
+    lastDebounceTime = currentTime;
+    
+    ledStatus = ledStatus == HIGH ? LOW : HIGH;
+    digitalWrite(13, ledStatus);
+    Serial.println(ledStatus);
+  }
+}
+
+void loop(){
+  int switchStatus = digitalRead(2);
+  
+  if(switchStatus == HIGH){
+    updateLed();
+  }
+}
+
---
+++ SwitchLed/SwitchLedBounceLibrary.ino
@@ -0,0 +1,18 @@
+#include <Bounce.h>
+
+Bounce bouncer = Bounce(2, 50);
+static int ledStatus = LOW;
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop(){
+  if(bouncer.update() == true && bouncer.read() == HIGH) {
+    ledStatus = ledStatus == HIGH ? LOW : HIGH;
+    digitalWrite(13, ledStatus);
+  }
+}
+
