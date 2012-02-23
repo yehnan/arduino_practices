c5e674ee6c9647ca15f25605c9af365c79f12e2e
Yehnan Chiang <yehnan174@gmail.com>
2012-02-23 10:38:25 +0800

    PhotoResistor

---
+++ PhotoResistor/PhotoResistor.ino
@@ -0,0 +1,22 @@
+/* Use a photo resistor to control a LED.
+  Details and schematic, please see http://yehnan.blogspot.com/2012/02/arduino_23.html
+*/
+int pr_min = 400;
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(A0, INPUT);
+  pinMode(13, OUTPUT);
+  
+  digitalWrite(13, LOW);
+}
+
+void loop(){
+  int pr = analogRead(A0);
+  Serial.println(pr);
+  
+  digitalWrite(13, pr > pr_min ? LOW : HIGH);
+  
+  delay(1000);
+}
+
