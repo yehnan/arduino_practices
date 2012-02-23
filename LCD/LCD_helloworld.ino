5bf2ab7794ffe6d48c5da96e5be76dc86366d701
Yehnan Chiang <yehnan174@gmail.com>
2012-02-23 16:06:25 +0800

    LCD init

---
+++ LCD/LCD_helloworld.ino
@@ -0,0 +1,19 @@
+/* Use LiquidCrystal library to control 2x16 LCD(Liquid Crystal Display)
+  Details please see 
+  http://yehnan.blogspot.com/2012/02/arduinoliquidcrystallcdhitachi-hd44780.html
+*/
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 2, 3, 4, 5);
+
+void setup(){
+  lcd.begin(16, 2);
+  lcd.print("Hello, World!");
+}
+
+void loop(){
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000);
+}
+
