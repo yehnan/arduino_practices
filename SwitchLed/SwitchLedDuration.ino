cce32d0fcd384ddbca242d9432b64a9da1a37bd6
Yehnan Chiang <yehnan174@gmail.com>
2012-08-25 21:29:32 +0800

    update SwitchLed

---
+++ SwitchLed/SwitchLedDuration.ino
@@ -0,0 +1,28 @@
+/* Use a switch to control a LED.
+  Details and schematic, please see http://yehnan.blogspot.com/2012/02/arduinoled.html
+*/
+#include <Bounce.h>
+
+Bounce bouncer = Bounce(2, 50);
+static int ledStatus = LOW;
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, ledStatus);
+}
+
+void loop(){
+  bouncer.update();
+  if(bouncer.read() == HIGH) {
+    ledStatus = bouncer.duration() >= 5000 ? LOW : HIGH;
+  }
+  else{
+    ledStatus = LOW;
+  }
+
+  digitalWrite(13, ledStatus);
+}
+
+
