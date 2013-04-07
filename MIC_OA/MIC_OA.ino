f340a9b700b6455fbc5b92d9f5abbde27ab3fb0f
Yehnan Chiang <yehnan174@gmail.com>
2013-04-07 18:43:33 +0800

    microphone and LM358

---
+++ MIC_OA/MIC_OA.ino
@@ -0,0 +1,22 @@
+
+int MIC_PIN = A0;
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(MIC_PIN, INPUT);     
+}
+int count = 0;
+void loop()
+{
+  int mic = analogRead(MIC_PIN);
+  
+  if(mic >= 500){
+    count++;
+    Serial.print("mic >= 500, count=");
+    Serial.println(count);
+  }
+
+  //delay(100);
+}
+
