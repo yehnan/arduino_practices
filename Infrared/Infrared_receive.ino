502e2a13c8c71d00cce26cbdf6cd5878c9538ff5
Yehnan Chiang <yehnan174@gmail.com>
2013-05-07 20:56:54 +0800

    Infrared

---
+++ Infrared/Infrared_receive.ino
@@ -0,0 +1,24 @@
+#include <IRremote.h>
+
+int RECV_PIN = 2;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup()
+{
+  Serial.begin(115200);
+  irrecv.blink13(true);
+  irrecv.enableIRIn();
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.print("results value is ");
+    Serial.print(results.value, HEX);
+    Serial.print(", bits is ");
+    Serial.print(results.bits);
+    Serial.print(", decode_type is ");
+    Serial.println(results.decode_type);
+    irrecv.resume();
+  }
+}
---
+++ Infrared/Infrared_send.ino
@@ -0,0 +1,43 @@
+#include <IRremote.h>
+
+IRsend irsend; // Digital Pin 3
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop() {
+  int d;
+  if( (d = Serial.read()) != -1) {
+    unsigned long v = 0x0;
+    switch(d){
+      case '1':
+        v = 0x77E14050;
+      break;
+      case '2':
+        v = 0x77E12050;
+      break;
+      case '3':
+        v = 0x77E1D050;
+      break;
+      case '4':
+        v = 0x77E1B050;
+      break;
+      case '5':
+        v = 0x77E1E050;
+      break;
+      case '6':
+        v = 0x77E11050;
+      break;
+    }
+    
+    if(v != 0x0){
+      Serial.print("read ");
+      Serial.print(d);
+      Serial.print(", IR send ");
+      Serial.println(v, HEX);
+      irsend.sendNEC(v, 32);
+    }
+  }
+}
