cfe32865d74d5e5f614f5437c797e46594583881
Yehnan Chiang <yehnan174@gmail.com>
2013-09-01 10:19:30 +0800

    Servo

---
+++ Servo1/Servo1.ino
@@ -0,0 +1,21 @@
+#include <Servo.h> 
+ 
+Servo myservo;
+
+void setup() 
+{ 
+  myservo.attach(9);
+} 
+
+void loop() 
+{
+  for(int i = 0; i <= 180; i+=1){
+    myservo.write(i);
+    delay(20);
+  }
+  for(int i = 180; i >= 0; i-=1){
+    myservo.write(i);
+    delay(20);
+  }
+}
+
---
+++ Servo2/Servo2.ino
@@ -0,0 +1,25 @@
+#include <Servo.h> 
+ 
+Servo myservo;
+
+void setup() 
+{ 
+  myservo.attach(9, 500, 2400);
+  myservo.write(90);
+  delay(3000);
+} 
+
+void loop() 
+{
+  for(int i = 500; i <= 2400; i+=100){
+    myservo.writeMicroseconds(i);
+    Serial.println(i);
+    delay(300);
+  }
+  for(int i = 2400; i >= 500; i-=100){
+    myservo.writeMicroseconds(i);
+    Serial.println(i);
+    delay(300);
+  }
+}
+
