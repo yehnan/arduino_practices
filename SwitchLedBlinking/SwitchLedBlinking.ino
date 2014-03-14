7109017e24c8f523a6c1d9bd220703125d739cf8
Yehnan Chiang <yehnan174@gmail.com>
2014-03-14 23:27:03 +0800

    use switch to control whether led blinks

---
+++ SwitchLedBlinking/SwitchLedBlinking.ino
@@ -0,0 +1,43 @@
+#define LED_PIN 13
+#define SWITCH_PIN 12
+
+#define DEBOUNCE_DELAY 200
+unsigned long lastDebounceTime;
+int ledBlinkStatus;
+
+#define LED_BLINK_TIME 300
+unsigned long lastUpdateLedTime;
+int ledStatus = LOW;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(SWITCH_PIN, INPUT);
+}
+void updateLedBlinkStatus(){
+  unsigned long currentTime = millis();
+  if((currentTime - lastDebounceTime) > DEBOUNCE_DELAY){
+    lastDebounceTime = currentTime;
+    ledBlinkStatus = !ledBlinkStatus;
+  }
+}
+void updateLed(){
+  if(ledBlinkStatus){
+    unsigned long currentTime = millis();
+    if((currentTime - lastUpdateLedTime) > LED_BLINK_TIME){
+      lastUpdateLedTime = currentTime;
+      ledStatus = !ledStatus;
+    }
+  }
+  else{
+    ledStatus = LOW;
+  }
+  digitalWrite(LED_PIN, ledStatus);
+}
+void loop() {
+  int switchStatus = digitalRead(SWITCH_PIN);
+  if(switchStatus == HIGH){
+    updateLedBlinkStatus();
+  }
+  updateLed();
+}
+
---
+++ SwitchLedBlinkingLib/SwitchLedBlinkingLib.ino
@@ -0,0 +1,33 @@
+#include <Bounce.h>
+#include <Timer.h>
+
+#define LED_PIN 13
+#define SWITCH_PIN 12
+
+#define DEBOUNCE_DELAY 50
+int ledBlinkStatus = 0;
+Bounce bouncer = Bounce(SWITCH_PIN, DEBOUNCE_DELAY);
+
+#define LED_BLINK_TIME 300
+Timer timer;
+int8_t timer_id;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(SWITCH_PIN, INPUT);
+}
+void loop() {
+  if(bouncer.update() == true && bouncer.read() == HIGH){
+    ledBlinkStatus = !ledBlinkStatus;
+    if(ledBlinkStatus){
+      timer_id = timer.oscillate(LED_PIN, LED_BLINK_TIME, HIGH);
+    }
+    else{
+      timer.stop(timer_id);
+      digitalWrite(LED_PIN, LOW);
+    }
+  }
+
+  timer.update();
+}
+
